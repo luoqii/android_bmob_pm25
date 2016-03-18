014549589e45805b377d644ff46c4fca25837f64
bb.S <bangbang.S@gmail.com>
2016-03-18 10:28:11 +0800

    add chart.

---
+++ app/src/main/java/org/bbs/android/bmob/pm25/App.java
@@ -0,0 +1,20 @@
+package org.bbs.android.bmob.pm25;
+
+import android.app.Application;
+
+import cn.bmob.v3.Bmob;
+
+/**
+ * Created by bysong on 16-3-17.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Bmob.DEBUG = true;
+        // bangbang.s
+        Bmob.initialize(this, "5543fd2c5165876ee945a6e2dae71718");
+    }
+}
--- app/src/main/java/org/bbs/android/bmob/pm25/MainActivity.java
+++ app/src/main/java/org/bbs/android/bmob/pm25/MainActivity.java
@@ -1,24 +1,42 @@
 package org.bbs.android.bmob.pm25;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Toast;
 
-import java.util.Random;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-import cn.bmob.v3.Bmob;
-import cn.bmob.v3.listener.SaveListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +47,6 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-
-        Bmob.DEBUG = BuildConfig.DEBUG;
-        Bmob.initialize(this, "4d4e1d0aceaebfd7013592cc4b334c84");
     }
 
 
@@ -57,12 +72,71 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
+   /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
 
+        public static final String PM_2_5 = "pm 2.5";
+        public static final String PM_1_0 = "pm 1.0";
+        public static final String PM_0_3 = "pm 0.3";
+        public static final String VALUE_0_3 = "value 0.3";
+        private LineChart mChart;
+        List<PMS50003> mData;
+
         public PlaceholderFragment() {
+            setHasOptionsMenu(true);
+            mData = new ArrayList<>();
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            inflater.inflate(R.menu.menu_fragment_main, menu);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.togglle_03:
+                    toggle03();
+                    break;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        private void toggle03() {
+            LineData lineData = mChart.getData();
+            final int COUNT = lineData.getDataSetCount();
+            for (int i = 0 ; i < COUNT; i++){
+                ILineDataSet dataSet = lineData.getDataSetByIndex(i);
+                if (dataSet.getLabel().equalsIgnoreCase(VALUE_0_3)){
+                    lineData.removeDataSet(dataSet);
+                    lineData.notifyDataChanged();
+                    mChart.invalidate();
+                    return;
+                }
+            }
+
+            final int L = mData.size();
+            ArrayList<Entry> vals03 = new ArrayList<Entry>();
+            for (int i = 0; i < L; i++) {
+                Entry e = new Entry(mData.get(i).value_0_3, i);
+                vals03.add(e);
+            }
+            LineDataSet set03 = new LineDataSet(vals03, VALUE_0_3);
+            set03.setColor(Color.YELLOW);
+
+//            lineData.getDataSets().add(set03);
+            lineData.addDataSet(set03);
+
+            mChart.notifyDataSetChanged();
+            mChart.invalidate();
         }
 
         @Override
@@ -74,41 +148,75 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         public void onViewCreated(View view, Bundle savedInstanceState) {
-            view.findViewById(R.id.save).setOnClickListener(this);
+            mChart = (LineChart) view.findViewById(R.id.chart);
+            mChart.setDescription("pm 2.5");
             super.onViewCreated(view, savedInstanceState);
+
+            initData();
+        }
+
+        void initData() {
+            BmobQuery<PMS50003> query = new BmobQuery<PMS50003>();
+            query.setLimit(500);
+
+            query.findObjects(getActivity(), new FindListener<PMS50003>() {
+                @Override
+                public void onSuccess(List<PMS50003> datas) {
+                    mData.addAll(datas);
+                    final int L = mData.size();
+                    toast("查询成功：共" + datas.size() + "条数据。");
+
+                    ArrayList<Entry> valsPm25 = new ArrayList<Entry>();
+                    for (int i = 0; i < L; i++) {
+                        Entry e = new Entry(mData.get(i).pm2_5, i);
+                        valsPm25.add(e);
+                    }
+                    LineDataSet setPm25 = new LineDataSet(valsPm25, PM_2_5);
+                    setPm25.setColor(Color.RED);
+
+                    ArrayList<Entry> valsPm10 = new ArrayList<Entry>();
+                    for (int i = 0; i < L; i++) {
+                        Entry e = new Entry(mData.get(i).pm1_0, i);
+                        valsPm10.add(e);
+                    }
+                    LineDataSet setPm10 = new LineDataSet(valsPm10,  PM_1_0);
+                    // use the interface ILineDataSet
+                    ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+                    dataSets.add(setPm25);
+
+                    dataSets.add(setPm10);
+
+                    ArrayList<String> xVals = new ArrayList<String>();
+                    for (int i = 0; i < L; i++) {
+                        xVals.add(i + "");
+                    }
+                    LineData data = new LineData(xVals, dataSets);
+                    mChart.setData(data);
+                    mChart.invalidate();
+                }
+
+                @Override
+                public void onError(int code, String msg) {
+                    // TODO Auto-generated method stub
+                    toast("查询失败：" + msg);
+                }
+            });
         }
 
         @Override
         public void onClick(View v) {
-            PMS50003 pm = new PMS50003();
-            Random r = new Random();
-            pm.pm1_0_CF1 = r.nextInt(100);
-            pm.pm2_5_CF1 = r.nextInt(100);
-            pm.pm10_CF1 = r.nextInt(100);
+            switch (v.getId()) {
+                case R.id.save_batch:
+//                    saveBatch();
+                    break;
+                case R.id.save:
+//                    save();
+                    break;
+            }
+        }
 
-            pm.pm1_0 = r.nextInt(100);
-            pm.pm2_5 = r.nextInt(100);
-            pm.pm10 = r.nextInt(100);
-
-            pm.value_0_3 = r.nextInt(100);
-            pm.value_0_5 = r.nextInt(100);
-            pm.value_1 = r.nextInt(100);
-            pm.value_2_5 = r.nextInt(100);
-            pm.value_5 = r.nextInt(100);
-            pm.value_10 = r.nextInt(100);
-            pm.recordedTime = System.currentTimeMillis();
-
-            pm.save(getActivity(), new SaveListener() {
-                @Override
-                public void onSuccess() {
-                    Toast.makeText(getActivity(), "save success.", Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                public void onFailure(int i, String s) {
-                    Toast.makeText(getActivity(), "save failed. " + s, Toast.LENGTH_LONG).show();
-                }
-            });
+        void toast(String message) {
+            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
         }
     }
 }
